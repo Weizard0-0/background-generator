5deaa7e357a1d79b6adacc6d51a7eeeaf3b42a2f
S.W. HSU <r01522108@g.ntu.edu.tw>
2021-01-22 14:31:05 +0800

    add project files

---
+++ script.js
@@ -0,0 +1,27 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button=document.querySelector(".button");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function random(){
+	
+
+};
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+button.addEventListener("click", random);
